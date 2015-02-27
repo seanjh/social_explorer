15ff1c44b72f303c7c264b8f45bef963ce93cf01
Sean Herman <seanherman@gmail.com>
2015-02-27 01:57:44 -0500

    tweaks

--- runserver.py
+++ runserver.py
@@ -4,5 +4,7 @@ from server.explorer import app
 if __name__ == '__main__':
     if os.getenv('FLASK_ENV', None) == 'PRODUCTION':
         app.run(host='0.0.0.0', port=80)
+        print 'Running app on 0.0.0.0:80'
     else:
         app.run(debug=True, port=5000)
+        print 'Running app on localhost:5000'
